aad68d54b3303c2dcf2fc829611f95f011f7f792
Prime-Fum-Star <primefuntv881@gmail.com>
2025-01-31 00:06:17 +0530

    Update core.py

--- core.py
+++ core.py
@@ -154,7 +154,7 @@ def time_name():
 
 
 async def download_video(url,cmd, name):
-    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
+    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" --referer "https://www.google.com" --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
